a1806b4cf41b61acbb8092fd2b3aaea908cc8059
pdarley <pdarley@host.docker.internal>
2022-12-13 17:22:08 -0800

    Added tools and

---
+++ mysite/GasCan/tools.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 13, 2022
+
+@author: pdarley
+
+Holds any random tools used in GasCan
+'''
+
+def mached_name_choices(choices) -> list:
+    """ Functin to return a list of tuples that contains the origional list doubled
+    ['thing1', 'thing2'] becomes [('thing1', 'thing1'), ('thing2', thing2')] """
+    
+    tuple_choices = []
+    for choice in choices:
+        tuple_choices.append((choice, choice))
+        
+    return tuple_choices
